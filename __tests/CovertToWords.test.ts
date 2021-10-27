954c7e5810b91f76ff0c779da65dc649c059d440
benlittlenz <benlittle53@gmail.com>
2021-10-27 16:35:49 +1300

    Feat: Added HTML to parse input number to convert function

--- __tests/CovertToWords.test.ts
+++ __tests/CovertToWords.test.ts
@@ -1,8 +1,8 @@
-import { convertNumToWords } from "../src/ConvertToWords";
+import { convertNumToCurrency } from "../src/ConvertToWords";
 
 describe("Initialize convert number to words", () => {
   const wordsShouldEqual = (number: number, expected: string) => {
-    expect(convertNumToWords(number)).toBe(expected);
+    expect(convertNumToCurrency(number)).toBe(expected);
   };
 
   it("1.15 should return One Dollar and Fifteen Cents", () => {
@@ -21,7 +21,15 @@ describe("Initialize convert number to words", () => {
     wordsShouldEqual(50, "Fifty Dollars");
   });
 
+  it("100 should return One Hundred Dollars", () => {
+    wordsShouldEqual(100, "One Hundred Dollars");
+  });
+
   it("200 should return Two Hundred Dollars", () => {
     wordsShouldEqual(200, "Two Hundred Dollars");
+  });
+
+  it("1000 should return One Hundred Dollars", () => {
+    wordsShouldEqual(1000, "One Thousand Dollars");
   });
 });
--- src/ConvertToWords.ts
+++ src/ConvertToWords.ts
@@ -1,27 +1,40 @@
 import { NumberWordMap } from "./types";
 import { numberWordMapping, toFixed, isFloat } from "./lib";
 
-export function convertNumToWords(number: number): string {
-  const words: string[] = convertToCurrency(number);
-  console.log("WORDS >>>> ", words);
-  return words.join(" ");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const convertButton = document.getElementById(
+    "convertBtn"
+  ) as HTMLFormElement;
+  const resultContainer = document.getElementById(
+    "resultContainer"
+  ) as HTMLFormElement;
 
-function convertToCurrency(number: number): string[] {
+  convertButton.addEventListener("click", () => {
+    const convertNumber = document.getElementById(
+      "converInput"
+    ) as HTMLFormElement;
+    const result = convertNumToCurrency(Number(convertNumber.value));
+
+    resultContainer.textContent = result;
+  });
+});
+
+export function convertNumToCurrency(number: number): string {
+  console.log("convertNumToWords NUMBER ", number);
   const splitNumber = String(toFixed(number)).split(".");
 
   const words = [...handleConvert(Number(splitNumber[0]))];
   Number(splitNumber[0]) === 1 ? words.push("Dollar") : words.push("Dollars");
 
+  // Check if input number contains decimals
   const isFloatingNumber = isFloat(number);
-
   if (isFloatingNumber) {
     words.push("and");
     words.push(...handleConvert(Number(splitNumber[1])));
     words.push("Cents");
   }
 
-  return words;
+  return words.join(" ");
 }
 
 function handleConvert(number: number): string[] {
@@ -31,22 +44,32 @@ function handleConvert(number: number): string[] {
     (elem) => Number(number) >= elem.number
   ) as NumberWordMap;
 
-  if (number <= 100) {
-    words.push(wordMatch.value);
-    number -= wordMatch.number;
-    if (number > 0) {
-      words.push(...handleConvert(number));
-    }
-    return words;
-  }
-
   const quotient = Math.floor(number / wordMatch.number);
-  console.log("quotient >>> ", quotient);
-  console.log("WORD MATCH", wordMatch.number);
+
   // Remaining value i.e. 33 % 30 === 1.1
   const remaining = number % wordMatch.number;
 
+  const powerOfTen = number % 10 === 0;
+  console.log("powerOfTen >>>> ", powerOfTen);
+  console.log("quotient >>>> ", quotient);
+  console.log("remaining >>>> ", remaining);
+
   if (quotient === 1) {
+    /*
+      When a number is a power of 10 and starts with 1 (i.e. 100 or 1000) just the Hundred or Thousand value is returned. To resolve this, we check if
+      A. the word match value is a power of 10
+      B. the number starts with a 1
+
+      If both statements are correct, we call the handleConvert function again
+    */
+    if (Number(wordMatch.number) % 10 === 0) {
+      console.log("IS POWER OF 10");
+      const firstNumber = Number(String(number)[0]);
+      if (firstNumber === 1) {
+        console.log("firstNumber", firstNumber);
+        words.push(...handleConvert(firstNumber));
+      }
+    }
     words.push(wordMatch.value);
   } else {
     words.push(...handleConvert(quotient), wordMatch.value);
